e430cdae927c9d3deb62d636e1640f77651b17a5
nicolai.negru <nicolai.negru@securethingz.com>
2020-07-24 18:25:19 +0100

    add more details

--- data_explorer.py
+++ data_explorer.py
@@ -47,3 +47,48 @@ def get_temperature_to_pressure_pairs(records):
 def get_pressure_to_wind_speed_pairs(records):
     zipped = [(x["main"]["pressure"], x["wind"]["speed"]) for x in records]
     return list(zip(*zipped))
+
+
+def get_temperature_to_lat_pairs(records):
+    zipped = [(x["main"]["temp"], x["city"]["coord"]["lat"]) for x in records]
+    return list(zip(*zipped))
+
+
+def get_temperature_to_distance_triplets(records):
+    zipped = [(x["main"]["temp"], x["distance"], x["city"]["country"]) for x in records]
+    return list(zip(*zipped))
+
+
+def get_distance_between(a, b):
+    """
+    source: https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude
+    """
+    from math import sin, cos, sqrt, atan2, radians
+
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat_a = radians(a["lat"])
+    lon_a = radians(a["lon"])
+    lat_b = radians(b["lat"])
+    lon_b = radians(b["lon"])
+
+    lon_delta = lon_b - lon_a
+    lat_delta = lat_b - lat_a
+
+    A = sin(lat_delta / 2)**2 + cos(lat_a) * cos(lat_b) * sin(lon_delta / 2)**2
+    c = 2 * atan2(sqrt(A), sqrt(1 - A))
+
+    return R * c
+
+def get_points_within_distance(center, max_distance_km, records):
+
+    # we can optimise this for performance
+    results = []
+    for x in records:
+        distance = get_distance_between(center, x["city"]["coord"])
+        if distance < max_distance_km:
+            x["distance"] = distance
+            results.append(x)
+
+    return results
